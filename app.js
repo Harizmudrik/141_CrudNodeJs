50936bb60cccc049cfc2cf83f47d3fa2ebd2b92c
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-11-13 23:06:22 +0700

    penambahan layout dan view

--- app.js
+++ app.js
@@ -4,16 +4,37 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
 
+const expressLayout = require(`express-ejs-layouts`)
+
+const db = require('./database/db');
+
+app.use(expressLayout);
+
 app.use(express.json());
 
 app.use('/todos', todoRoutes);
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        layout: 'layouts/main-layout'
+    });
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', {
+        layout: 'layouts/main-layout'
+    });
+    
+});
+
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
 });
 
 app.use((req, res) => {
